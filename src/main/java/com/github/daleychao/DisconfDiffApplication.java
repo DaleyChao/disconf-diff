060a64e10473f2447bf2584778c0e4cf909570cd
Daniel <super.daichao@gmail.com>
2018-08-30 09:42:04 +0800

    Initial commit

---
+++ src/main/java/com/github/daleychao/DisconfDiffApplication.java
@@ -0,0 +1,12 @@
+package com.github.daleychao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisconfDiffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisconfDiffApplication.class, args);
+	}
+}
